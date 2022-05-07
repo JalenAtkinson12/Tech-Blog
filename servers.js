2f32fef5a7469bba4c7f653003bc9436e7fb0da9
Jalen Atkinson <Jalenatkinson35@gmail.com>
2022-05-07 14:31:43 -0400

    stated on the server file, and all of the connections

---
+++ servers.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const sessi = {
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sessi));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(route);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+});
