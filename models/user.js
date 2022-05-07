74faf83729071a4e6c5dd5853f92da6f69c0498c
Jalen Atkinson <Jalenatkinson35@gmail.com>
2022-05-07 17:56:00 -0400

    updated the models folder

--- models/user.js
+++ models/user.js
@@ -10,6 +10,12 @@ class User extends Model {
 
 User.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -25,15 +31,27 @@ User.init(
       },
     },
   },
+
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
     modelName: "user",
   }
 );
-
-user.addHook("beforeCreate", async (user) => {
-  user.password = await bcrypt.hash(user.password, 10);
-  return user;
-});
 
 module.exports = User;
